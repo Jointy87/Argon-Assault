ff15bb0a3cb20f2d31a4d327bf82b5a91e5b08a6
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-03-31 23:08:46 +0200

    stuff

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        if(currentScene.name == "Splash")
+        {
+            Invoke("LoadNextScene", 2f);
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadSceneAsync(currentSceneIndex + 1);
+    }
+
+}
