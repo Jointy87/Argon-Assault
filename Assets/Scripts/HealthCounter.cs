3cf74a917af20ef5d4ef559b77bedbf90af8593c
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-04-02 22:26:36 +0200

    Add: Created script

---
+++ Assets/Scripts/HealthCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCounter : MonoBehaviour
+{
+	//Parameters
+	[Tooltip("The amount of health the object has")][SerializeField] int healthPoints;
+
+	//Cache
+	int currentHealth;
+
+	private void Start()
+	{
+		currentHealth = healthPoints;
+		print(currentHealth);
+	}
+
+	public void SubstractHealth()
+	{
+		currentHealth--;
+	}
+	public int FetchHealth()
+	{
+		return currentHealth;
+	}
+}
