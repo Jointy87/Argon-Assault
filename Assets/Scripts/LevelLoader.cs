be7e9ebef95bf0130200b6753dcc9dd49308d144
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-04-02 18:48:55 +0200

    ADD: added restartgame() to scripts. Made levelloader a prefab.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,20 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
-        if(currentScene.name == "Splash")
-        {
-            Invoke("LoadNextScene", 2f);
-        }
-    }
+	void Start()
+	{
+		Scene currentScene = SceneManager.GetActiveScene();
+		if(currentScene.name == "Splash")
+		{
+			Invoke("LoadNextScene", 2f);
+		}
+	}
 
-    public void LoadNextScene()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadSceneAsync(currentSceneIndex + 1);
-    }
+	public void LoadNextScene()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadSceneAsync(currentSceneIndex + 1);
+	}
 
+	public void RestartGame()
+	{
+		SceneManager.LoadSceneAsync("Splash");
+	}
 }
