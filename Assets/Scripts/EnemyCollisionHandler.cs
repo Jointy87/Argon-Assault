a474548159a348770ee2f67667fb687dcaef80a3
Jointy87 <j.s.barbosa.dijkstra@gmail.com>
2020-04-02 22:28:00 +0200

    ADD: added hit damage and hitFX field

--- Assets/Scripts/EnemyCollisionHandler.cs
+++ Assets/Scripts/EnemyCollisionHandler.cs
@@ -7,15 +7,19 @@ public class EnemyCollisionHandler : MonoBehaviour
 {
 	//Parameters
 	[SerializeField] GameObject deathFX;
+	[SerializeField] GameObject hitFX;
 	[Tooltip("The parent under which the deathFX will spawn during Runtime")]
-	[SerializeField] Transform deathFXParent;
+	[SerializeField] Transform parentFX;
 	[Tooltip("The amount of points scored upon destruction")] [SerializeField] int pointWorth;
 
 	//Cache
 	bool isAlive;
+	HealthCounter healthCounter;
+	int currentHealth;
 	private void Start()
 	{
 		isAlive = true;
+		healthCounter = GetComponent<HealthCounter>();
 		AddBoxCollider();
 	}
 
@@ -26,10 +30,17 @@ public class EnemyCollisionHandler : MonoBehaviour
 
 	private void OnParticleCollision(GameObject other)
 	{
-		while (isAlive) //To avoid counting double score if being hit by both lasers
+		if (healthCounter.FetchHealth() <= 0)
 		{
-			StartDeathSequence();
-			isAlive = false;
+			while (isAlive) //To avoid counting double score if being hit by both lasers
+			{
+				StartDeathSequence();
+				isAlive = false;
+			}
+		}
+		else
+		{
+			healthCounter.SubstractHealth();
 		}
 	}
 
@@ -40,10 +51,10 @@ public class EnemyCollisionHandler : MonoBehaviour
 		DestroyShip();
 	}
 
-	private void TriggerDeathFX()
+	private void TriggerDeathFX() //todo fix bug where explosion sound doesn't seem to be playing on enemies
 	{
 		GameObject explosionFX = Instantiate(deathFX, transform.position, Quaternion.identity);
-		explosionFX.transform.parent = deathFXParent;
+		explosionFX.transform.parent = parentFX;
 		Destroy(explosionFX, 1f);
 	}
 	private void SendScoreToCounter()
